b81b4af4f82e51af5c7caf681347b8a83d12771f
personallyvnguyen <60557484+personallyvnguyen@users.noreply.github.com>
2023-03-08 15:25:08 -0800

    moved cdk to own repo, added new subpath

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+const subpath = "/api/v1/"
+
+app.get(`${subpath}`, (req, res) => {
   res.send("Hello World 2/10/23 2:52 AM!");
 });
 
