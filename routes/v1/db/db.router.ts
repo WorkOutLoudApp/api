f59e44f7c7cfca23a5242c4ba65d362661d3836d
personallyvnguyen <60557484+personallyvnguyen@users.noreply.github.com>
2023-03-11 16:01:27 -0800

    test if ECS can use sdk

---
+++ routes/v1/db/db.router.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { getHello } from '../../../controllers/hello.controller';
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "wol-app-db-dev-secret";
+
+const client = new SecretsManagerClient({
+  region: "us-west-2",
+});
+
+let response;
+
+// Your code goes here
+
+const router = Router({ mergeParams: true })
+
+router.get('/secret', async (req, res) => {
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+    const secret = response.SecretString;
+
+    console.log(secret)
+    res.send(secret);
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    res.send(error);
+  }
+});
+
+export default router
