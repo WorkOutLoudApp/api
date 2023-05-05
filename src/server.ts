bf19a7e7e91ef31b37ff4e8d78b640b71ec34dad
personallyvnguyen <60557484+personallyvnguyen@users.noreply.github.com>
2023-05-05 02:04:52 -0700

    reordered api healthcheck

--- src/server.ts
+++ src/server.ts
@@ -15,16 +15,15 @@ if (dev) app.use(morgan('dev'))
 // Routes
 app.use('/api/v1', v1Router)
 
+// For AWS ECS to know health of API
+app.get("/api/healthcheck", (req, res) => {
+  res.sendStatus(200);
+});
 
 // Error Handling
 app.use((req: Request, res: Response) => {
   res.status(404).send('Not Found')
 })
-
-// For AWS ECS to know health of API
-app.get("/api/healthcheck", (req, res) => {
-  res.sendStatus(200);
-});
 
 app.use(errorHandler)
 
